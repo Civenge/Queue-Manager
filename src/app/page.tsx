08453f04302181b11e5f06da5d7e19d7019c61f8
Kyle Greene <kmgreene@gmail.com>
2024-07-18 10:39:36 -0700

    Adjusted css styling for adding new class page and main title.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,7 +147,7 @@ const Home = () => {
       <ToastContainer />
       <main className="flex min-h-fit flex-col items-center justify-between p-24">
         <div className="py-2 px-4 text-blue-500 rounded-md focus:outline-none  text-center text-4xl font-bold">
-          Welcome to Kyle&apos;s Queue Manager
+          Welcome to the TA Queue Manager
         </div>
         <div>
           <form onSubmit={handlePageCreation} className="mt-4 py-2">
@@ -155,15 +155,15 @@ const Home = () => {
               type="text"
               value={newPageName}
               onChange={(e) => setNewPageName(e.target.value)}
-              className="py-2 px-4 mr-2 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none focus:border-blue-500 dark:bg-gray-800 dark:text-gray-300"
-              placeholder="Enter new page name here."
+              className="py-2 px-4 mr-2 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none focus:border-blue-500 dark:bg-gray-800 dark:text-gray-300 min-w-[275px]"
+              placeholder="Enter new classroom number."
               required
             ></input>
             <button
               type="submit"
               className="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
             >
-              Create Page
+              Add New Classroom
             </button>
           </form>
         </div>
